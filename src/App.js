cc509ed73471f70a1ad7edc189ea029ba80965d2
João Pedro de Sousa Cruz <jolunyna@gmail.com>
2022-07-16 12:07:52 -0300

    edit item 1

--- src/App.js
+++ src/App.js
@@ -38,6 +38,12 @@ function App() {
     showAlert(true, 'danger', 'item removed');
     setList(list.filter((item) => item.id !== id));
   };
+  const editItem = (id) => {
+    const specificItem = list.find((item) => item.id == item);
+    setIsEditing(true);
+    setEditID(id);
+    setName(specificItem.title);
+  };
 
   return (
     <section className='section-center'>
@@ -59,7 +65,7 @@ function App() {
       </form>
       {list.length > 0 && (
         <div className='grocery-container'>
-          <List items={list} removeItem={removeItem} />
+          <List items={list} removeItem={removeItem} editItem={editItem} />
           <button className='clear-btn' onClick={clearList}>
             clear items
           </button>
